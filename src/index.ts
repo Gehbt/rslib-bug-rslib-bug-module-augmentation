431a29125aa08d4628d28e2150af5a50bf3e853b
rgehbt <rgehbt@gmail.com>
2026-03-14 14:54:34 +0800

    chore: add comment and rename

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { type Node } from '#lib';
 
+// Path alias doesn't seem to be what's causing the error.
 declare module "#lib" {
     export interface Node {
         visitor: (node: Node) => unknown
